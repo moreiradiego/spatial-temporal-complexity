4effab0092bec3d8de8e7bbfa1b4a03716f71636
Diego Moreira <diego@moreira.bio.br>
2023-07-21 18:15:30 -0300

    Start to create dynamic samples

--- algo_01.py
+++ algo_01.py
@@ -48,19 +48,38 @@ def calculate_max_mem_and_time_usage(function_to_evaluate, data_to_be_processed,
         data["max_memory_usage_mib"] = result["memory_delta"] if result["memory_delta"] > data[
             "max_memory_usage_mib"] else data["max_memory_usage_mib"]
         data["max_time_enlapsed"] = result["memory_delta"] if result["time_enlapsed"] > data["max_time_enlapsed"] else \
-        data["max_time_enlapsed"]
+            data["max_time_enlapsed"]
     data["max_memory_usage_kib"] = data["max_memory_usage_mib"] * 1000
     return data
 
 
 if __name__ == '__main__':
-    list_to_be_processed = random.randint(0, 100, 1000)
+    # import dill as pickle
+
+    list_to_be_processed = random.randint(0, 100, 1)
     times_to_iterate = 100
-    print(show_equals_in_list.__name__, calculate_max_mem_and_time_usage(show_equals_in_list, list_to_be_processed,
-                                                                         times_to_iterate=times_to_iterate))
-    print(show_equals_list_iteration_utilities.__name__,
-          calculate_max_mem_and_time_usage(show_equals_list_iteration_utilities, list_to_be_processed,
-                                           times_to_iterate=times_to_iterate))
-    print(show_equals_iterations_w_numpy.__name__,
-          calculate_max_mem_and_time_usage(show_equals_iterations_w_numpy, list_to_be_processed,
-                                           times_to_iterate=times_to_iterate))
+    # print(show_equals_in_list.__name__, calculate_max_mem_and_time_usage(show_equals_in_list, list_to_be_processed,
+    #                                                                      times_to_iterate=times_to_iterate))
+    # print(show_equals_list_iteration_utilities.__name__,
+    #       calculate_max_mem_and_time_usage(show_equals_list_iteration_utilities, list_to_be_processed,
+    #                                        times_to_iterate=times_to_iterate))
+    # print(show_equals_iterations_w_numpy.__name__,
+    #       calculate_max_mem_and_time_usage(show_equals_iterations_w_numpy, list_to_be_processed,
+    #                                        times_to_iterate=times_to_iterate))
+    from dill import loads, dumps, source
+
+    squared = lambda x: x ** 2
+    aux_to_print = source.getsourcelines(calculate_max_mem_and_time_usage)
+    # list_obj = [x for x in list(aux_to_print)]
+    print(aux_to_print)
+    # print(source.getsourcelines(calculate_max_mem_and_time_usage))
+b = (['def calculate_max_mem_and_time_usage(function_to_evaluate, data_to_be_processed, times_to_iterate):\n',
+      '    data = {"max_memory_usage_mib": 0, "max_memory_usage_kib": 0, "max_time_enlapsed": 0}\n',
+      '    for _ in range(times_to_iterate):\n',
+      '        result = profile_complexity_function(function_to_evaluate, *data_to_be_processed)\n',
+      '        data["max_memory_usage_mib"] = result["memory_delta"] if result["memory_delta"] > data[\n',
+      '            "max_memory_usage_mib"] else data["max_memory_usage_mib"]\n',
+      '        data["max_time_enlapsed"] = result["memory_delta"] if result["time_enlapsed"] > data["max_time_enlapsed"] else \\\n',
+      '            data["max_time_enlapsed"]\n',
+      '    data["max_memory_usage_kib"] = data["max_memory_usage_mib"] * 1000\n', '    return data\n'],
+     43)
